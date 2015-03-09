971f1afebdc8ec277f82ed79e123f351a001a97c
dpaschal <daniel.paschal@gmail.com>
2015-03-09 14:39:40 -0700

    added some php and changed name

--- tableversion.php
+++ tableversion.php
@@ -1,4 +1,18 @@
 <!doctype html>
+<?php
+$students = [
+    ['name'=>'George Takei', 'Class'=>'Warp Physics', 'Grade'=>75],
+    ['name'=>'Leonard Nimoy', 'Class'=>'Anger Management', 'Grade'=>98],
+    ['name'=>'William Shatner', 'Class'=>'Ethics & the Chain of Command', 'Grade'=>69],
+    ['name'=>'James Doohan', 'Class'=>'Warp Physics', 'Grade'=>92],
+    ['name'=>'George Takei', 'Class'=>'Piloting', 'Grade'=>95],
+    ['name'=>'Leonard Nimoy', 'Class'=>'Warp Physics', 'Grade'=>95],
+    ['name'=>'Deforest Kelley', 'Class'=>'Botony', 'Grade'=>85],
+    ['name'=>'Nichelle Nichols', 'Class'=>'Communications', 'Grade'=>95],
+    ['name'=>'Zoe Saldana', 'Class'=>'Communications', 'Grade'=>35],
+    ['name'=>'William Shatner', 'Class'=>'Trible Care', 'Grade'=>100],
+];
+?>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -29,11 +43,16 @@
             <td>Anger Management</td>
             <td>98</td>
         </tr>
-        <tr>
+<?php
+    for($i = 0; $i<count($students); $i++)
+    {
+        echo "<tr>
             <td>William Shatner</td>
             <td>Ethics & the Chain of Command</td>
             <td>69</td>
-        </tr>
+        </tr>";
+    }
+?>
     </table>
 
 
