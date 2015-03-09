493a1e3a0495410ea6feff2de4f10f1911443396
dpaschal <daniel.paschal@gmail.com>
2015-03-09 14:29:16 -0700

    base html table

---
+++ tableversion.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Student Grade Table PHP</title>
+<style>
+#student_grade_table td{
+    padding: 5px;
+}
+#student_grade_table tr:nth-child(even){
+    background-color: lightgray;;
+}
+</style>
+</head>
+<body>
+    <table id="student_grade_table">
+        <tr>
+            <td>Name</td>
+            <td>Class</td>
+            <td>Grade</td>
+        </tr>
+        <tr>
+            <td>George Takei</td>
+            <td>Warp Physics</td>
+            <td>75</td>
+        </tr>
+        <tr>
+            <td>Leonard Nimoy</td>
+            <td>Anger Management</td>
+            <td>98</td>
+        </tr>
+        <tr>
+            <td>William Shatner</td>
+            <td>Ethics & the Chain of Command</td>
+            <td>69</td>
+        </tr>
+    </table>
+
+
+</body>
+</html>
